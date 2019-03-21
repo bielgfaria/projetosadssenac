521392f2aa46a3bf9cf9bcedc959fad367402e90
Alunos <Alunos@PLE109D29.alunos.sc.senac.br>
2019-03-20 21:52:28 -0300

    Primeirocomitmeu

---
+++ src/ExerciciosDia20/joptionpane3.java
@@ -0,0 +1,21 @@
+package ExerciciosDia20;
+
+import javax.swing.JOptionPane;
+
+public class joptionpane3 {
+
+    public static void main(String[] args) {
+        String s = JOptionPane.showInputDialog("Digite o primeiro numero");
+        int n1 = Integer.valueOf(s);
+        s = JOptionPane.showInputDialog("Digite o segundo numero");
+        int n2 = Integer.valueOf(s);
+        s = JOptionPane.showInputDialog("Digite o terceiro numero");
+        int n3 = Integer.valueOf(s);
+        s = JOptionPane.showInputDialog("Digite o quarto numero");
+        int n4 = Integer.valueOf(s);
+        s = JOptionPane.showInputDialog("Digite o quinto numero");
+        int n5 = Integer.valueOf(s);
+
+    }
+
+}
