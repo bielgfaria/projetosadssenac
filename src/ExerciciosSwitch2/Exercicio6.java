05b0f7c5e8ea7e20ed1e0998aceb3f7a8d983572
55489 <55489@LAPTOP-C72HNCD5>
2019-03-28 21:19:00 -0300

    meucommit

--- src/ExerciciosSwitch2/Exercicio6.java
+++ src/ExerciciosSwitch2/Exercicio6.java
@@ -8,6 +8,34 @@ caso contrário apresentar a mensagem que o passageiro não deve pagar nada.
  */
 package ExerciciosSwitch2;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio6 {
 
-}
+    public static void main(String[] args) {
+
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O PESO DA BAGAGEM:"));
+
+        String[] viagem = {"VIAGEM NACIONAL", "VIAGEM INTERNACIONAL"};
+
+        String valor1 = String.valueOf(JOptionPane.showInputDialog(
+                null, "Selecione uma opção abaixo:", "ESCOLHA", JOptionPane.QUESTION_MESSAGE, null,
+                viagem, viagem[1]));
+        if (valor1.equalsIgnoreCase("VIAGEM NACIONAL")) {
+            if (peso > 23.0) {
+                JOptionPane.showMessageDialog(null, "VALOR DO EXCESSO SERÁ: " + (peso - 23.0) * 3);
+            } else {
+                JOptionPane.showMessageDialog(null, "NAO PAGARA EXCESSO DE BAGAGEM.");
+            }}
+            if (valor1.equalsIgnoreCase("VIAGEM INTERNACIONAL")) {
+                if (peso > 64.0) {
+                    JOptionPane.showMessageDialog(null, "VALOR DO EXCESSO SERÁ: " + (peso - 64.0) * 5);
+                } else {
+                    JOptionPane.showMessageDialog(null, "NAO PAGARA EXCESSO DE BAGAGEM.");
+                }
+            }
+        }
+
+    }
+
+
